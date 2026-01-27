55cc5fa126aabcea40b8259e99e942890c9a49eb
FCC <aleenaiqbal1102@gmail.com>
2026-01-27 23:22:45 +0500

    Soft Assertion

--- TestCases/test_blogPage.py
+++ TestCases/test_blogPage.py
@@ -1,51 +1,34 @@
 import pytest
 from Pages.BlogPage import BlogPage
+import pytest_check as check
 from Config.conftest import setup
 
+@pytest.mark.blog
 class TestBlogPage:
     def setup_method(self):
         self.base_url = 'https://ginandjuice.shop/blog'
 
-    @pytest.mark.blog
-    def test_blog_page_load(self):
+
+    def test_blog_page_ui(self):
         blog = BlogPage(self.driver)
         blog.open_blog_page()
-        assert "blog" in self.driver.current_url
 
-    @pytest.mark.blog
-    def test_blog_page_title(self):
-        blog = BlogPage(self.driver)
-        blog.open_blog_page()
-        assert "Blog - Gin & Juice Shop" in self.driver.title
+        #---------Soft Assertion----------
 
-    @pytest.mark.blog
-    def test_blog_post_title_visible(self):
-        blog = BlogPage(self.driver)
-        blog.open_blog_page()
-        assert blog.first_post_title_displayed()
+        check.is_in("blog", self.driver.current_url,"Blog URL is incorrect")
+        check.is_in("Blog - Gin & Juice Shop", self.driver.title,"Blog page title is incorrect")
+        check.is_true(blog.first_post_title_displayed(), "First blog post title is not visible")
+        check.is_true(blog.first_post_image(), "First blog post image is not visible")
+        check.is_true(blog.first_post_description(), "First blog post description is not visible")
 
-    @pytest.mark.blog
-    def test_blog_post_image_visible(self):
-        blog = BlogPage(self.driver)
-        blog.open_blog_page()
-        assert blog.first_post_image()
 
-    @pytest.mark.blog
-    def test_blog_post_description_visible(self):
-        blog = BlogPage(self.driver)
-        blog.open_blog_page()
-        assert blog.first_post_description()
-
-    @pytest.mark.blog
     def test_search_blog(self):
         blog = BlogPage(self.driver)
         blog.open_blog_page()
         blog.search_blog("WHEEL")
 
-    @pytest.mark.blog
     def test_view_blog_post(self):
         blog = BlogPage(self.driver)
         blog.open_blog_page()
         blog.view_blog()
-
 
--- TestCases/test_ourstory.py
+++ TestCases/test_ourstory.py
@@ -1,19 +1,17 @@
 import pytest
 from Pages.OurStoryPage import OurStoryPage
+import pytest_check as check
 from Config.conftest import setup
 
 class TestOurStoryPage:
-    def setup_method(self):
-        self.base_url = 'https://ginandjuice.shop/about'
 
     @pytest.mark.story
-    def test_our_story_page_load(self):
+    def test_our_story_page(self):
         page = OurStoryPage(self.driver)
         page.open_page()
-        assert "about" in page.get_current_url()
 
-    @pytest.mark.story
-    def test_our_story_page_title(self):
-        page = OurStoryPage(self.driver)
-        page.open_page()
-        assert "Our story - Gin & Juice Shop" in page.get_page_title()
+        #--------Soft Assertion---------
+
+        check.is_in("about",page.get_current_url(), "URL does not contain 'about'")
+        check.is_in("Our story - Gin & Juice Shop", page.get_page_title(),
+                    "Page title is incorrect")
